c853ec129e8d331e2c5366cc52fb222ef4a931a0
envix <bevanswd@gmail.com>
2016-05-23 15:57:31 -0600

    todoApp
    todo list app

---
+++ todoApp/database/migrations/2016_05_23_195508_create_todo_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->boolean('done');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('todos');
+    }
+}
